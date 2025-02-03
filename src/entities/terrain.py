d9cf381658dfdd5c72c48acbd2ab25cdc7fa7b9a
Calvin <nkmcalvin@gmail.com>
2025-02-03 12:31:50 +0100

    change the terrain section

--- src/entities/terrain.py
+++ src/entities/terrain.py
@@ -1,2 +1,26 @@
-﻿class Terrain:
-    pass
+﻿import pygame
+
+
+class Terrain:
+    def __init__(self, screen, size: tuple, color: pygame.Color, image_path: str = None):
+        self._screen = screen
+        self._size = size
+        self._color = color
+        self._image_path = image_path
+
+        if self._image_path:
+            self._image = pygame.image.load(self._image_path).convert()
+            self._image = pygame.transform.scale(self._image, self._size)
+        else:
+            self._image = None
+
+    def draw(self):
+        if self._image:
+            self._screen.blit(self._image, (0, 0))
+        else:
+            self._screen.fill(self._color)
+
+        pygame.display.update()
+
+
+
