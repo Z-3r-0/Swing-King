5d33309554201729bcc11c2b4996f0ea4da91dca
Côme <come.deveze@efrei.net>
2025-02-03 13:49:17 +0100

    Added calculate traj x to know pos x in function of time

--- src/utils/physics_utils.py
+++ src/utils/physics_utils.py
@@ -19,3 +19,25 @@ def calculate_trajectory(x: float, force: float, angle: int):
 
     y = (-4.9 * (x ** 2)) / (force ** 2 * cos_alpha ** 2) + tan_alpha * x
     return y
+
+def calculate_traj_x(t: float, force: float, angle: int, mass: float):
+    """
+    Calculates the horizontal position (x) of the ball based on the time (t).
+
+    :param t: time elapsed since the impact
+    :param force: Applied force
+    :param angle: Launch angle (alpha) in degrees
+    :param mass: mass of the ball
+    :return: Horizontal position (x) of the ball
+    """
+
+    angle_radians = math.radians(angle)
+    cos_alpha = math.cos(angle_radians)
+
+    cos_alpha = 0.1 if cos_alpha == 0 else cos_alpha  # Avoid division by zero if angle is 90°
+
+    # print("Vo: ", (force*0.1/mass)*cos_alpha)
+
+    x = (force*0.1/mass)*cos_alpha * t
+
+    return x
